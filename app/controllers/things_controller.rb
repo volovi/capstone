02e8f2aced531e0643be88068e7f17501429b944
Alexey Volovikov <volovi@gmail.com>
2018-05-15 21:35:53 +0300

    server:thing authz policy enforcement

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,26 +1,45 @@
 class ThingsController < ApplicationController
+  include ActionController::Helpers
+  helper ThingsHelper
   before_action :set_thing, only: [:show, :update, :destroy]
-  wrap_parameters :thing, include: ["name", "description", "notes"]
   before_action :authenticate_user!, only: [:create, :update, :destroy]
+  wrap_parameters :thing, include: ["name", "description", "notes"]
+  after_action :verify_authorized
+  after_action :verify_policy_scoped, only: [:index]
 
   def index
-    @things = Thing.all
+    authorize Thing
+    things = policy_scope(Thing.all)
+    @things = ThingPolicy.merge(things)
   end
 
   def show
+    authorize @thing
+    things = ThingPolicy::Scope.new(current_user,
+                                    Thing.where(:id=>@thing.id))
+                                    .user_roles(false)
+    @thing = ThingPolicy.merge(things).first
   end
 
   def create
+    authorize Thing
     @thing = Thing.new(thing_params)
 
-    if @thing.save
-      render :show, status: :created, location: @thing
-    else
-      render json: {errors:@thing.errors.messages}, status: :unprocessable_entity
+    User.transaction do
+      if @thing.save
+        role=current_user.add_role(Role::ORGANIZER,@thing)
+        @thing.user_roles << role.role_name
+        role.save!
+        render :show, status: :created, location: @thing
+      else
+        render json: {errors:@thing.errors.messages}, status: :unprocessable_entity
+      end
     end
   end
 
   def update
+    authorize @thing
+
     if @thing.update(thing_params)
       head :no_content
     else
@@ -29,6 +48,7 @@ class ThingsController < ApplicationController
   end
 
   def destroy
+    authorize @thing
     @thing.destroy
 
     head :no_content
