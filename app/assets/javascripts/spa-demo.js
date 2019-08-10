f4e6439757353c5364437d52ed52fb3e70b83efd
Alexey Volovikov <volovi@gmail.com>
2019-08-10 21:11:33 +0300

    imageviewer displays image content

--- app/assets/javascripts/spa-demo.js
+++ app/assets/javascripts/spa-demo.js
@@ -9,7 +9,7 @@
 //= require ng-file-upload-shim
 //= require ng-file-upload
 //= require ui-cropper
- 
+
 //= require spa-demo/app.module
 //= require spa-demo/app.router
 
@@ -29,10 +29,12 @@
 //= require spa-demo/authz/base_policy.service
 
 //= require spa-demo/layout/layout.module
+//= require spa-demo/layout/image_query_size.service
 //= require spa-demo/layout/navbar/navbar.component
 //= require spa-demo/layout/image_loader/data_utils.service
 //= require spa-demo/layout/image_loader/image_loader.component
-
+//= require spa-demo/layout/image_viewer/image_viewer.component
+ 
 //= require spa-demo/foos/foos.module
 //= require spa-demo/foos/foos.service
 //= require spa-demo/foos/foos.controller
---
+++ app/assets/javascripts/spa-demo/layout/image_viewer/image_viewer.component.js
@@ -0,0 +1,49 @@
+(function() {
+  "use strict";
+
+  angular
+    .module("spa-demo.layout")
+    .component("sdImageViewer", {
+      templateUrl: templateUrl,
+      controller: ImageViewerController,
+      bindings: {
+        name: "@",
+        images: "<",
+      },
+    });
+
+  templateUrl.$inject = ["spa-demo.config.APP_CONFIG"];
+  function templateUrl(APP_CONFIG) {
+    return APP_CONFIG.image_viewer_html;
+  }    
+
+  ImageViewerController.$inject = ["$scope"];
+  function ImageViewerController($scope) {
+    var vm=this;
+    vm.imageUrl=imageUrl;
+    vm.imageId=imageId;
+    vm.isCurrentIndex=isCurrentIndex;
+
+    vm.$onInit = function() {
+      vm.currentIndex = 0;
+      console.log(vm.name, "ImageViewerController", $scope);
+    }
+    return;
+    //////////////
+    function isCurrentIndex(index) {
+      return index === vm.currentIndex;
+    }
+
+    function imageUrl(object) {
+      if (!object) { return null; }
+      var url = object.image_id ? object.image_content_url : object.content_url;
+      console.log(vm.name, "url=", url);
+      return url;
+    }
+    function imageId(object) {
+      if (!object) { return null }
+      var id = object.image_id ? object.image_id : object.id;
+      return id; 
+    }  
+  }
+})();
